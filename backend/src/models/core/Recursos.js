c6d1d152936beb50ac2dae97db6bb5b71f5c56ac
alejopxt <150355484+alejopxt@users.noreply.github.com>
2025-06-16 16:54:22 -0500

    Pruebas
    Fusion de todo para realizarla desde aqui

---
+++ backend/src/models/core/Recursos.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const recursoSchema = new mongoose.Schema({
+    idRecursos: {
+        type: Number,
+        required: true
+    },
+    nombreRecursos: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 45
+    },
+    cantidadRecursos: {
+        type: Number,
+        required: true
+    },
+    disponibilidadR: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 45
+    },
+    mantenimientoR: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 45
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Recurso', recursoSchema);
