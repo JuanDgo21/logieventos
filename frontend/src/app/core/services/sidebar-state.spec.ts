4aa6cecd5b06e5734a1240eefc010b629aa2ff06
Juana-Valentina <150572079+Juana-Valentina@users.noreply.github.com>
2025-11-17 01:03:37 -0500

    servicios completados

--- frontend/src/app/core/services/sidebar-state.spec.ts
+++ frontend/src/app/core/services/sidebar-state.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
-
 import { SidebarStateService } from './sidebar-state';
 
 describe('SidebarStateService', () => {
-  let service: SidebarStateService  ;
+  let service: SidebarStateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [SidebarStateService]
+    });
+
     service = TestBed.inject(SidebarStateService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should have isOpen = true by default', () => {
+    expect(service.isOpen).toBeTrue();
+  });
+
+  it('should toggle isOpen value', () => {
+    const initialState = service.isOpen;
+
+    service.toggle();
+
+    expect(service.isOpen).toBe(!initialState);
+  });
+
+  it('should toggle back to initial value after two toggles', () => {
+    const initialState = service.isOpen;
+
+    service.toggle(); // 1
+    service.toggle(); // 2
+
+    expect(service.isOpen).toBe(initialState);
   });
 });
